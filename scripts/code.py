7befdf320061059509caefda94ef7ab7fd18f865
adizelkin-dotcom <adi.zelkin@gmail.com>
2026-03-09 17:24:21 +0200

    final

--- scripts/code.py
+++ scripts/code.py
@@ -1,7 +1,20 @@
-file = open("Sickle_cell_freq_het.csv", "w")
+file = open("scripts/csv.Sickle_cell_freq_het", "w")
 q = 0.0036 ** 0.5
 p = 1 - q
 generations = 50
-header = "Generation,p(HbA),q(HbS)\n"
+header = "Generation\tp(HbA)\tq(HbS)\n"
 file.write(header)
 gen = 0
+
+while gen <= generations:
+
+    line = str(gen) + "\t\t\t" + str(p) + "\t" + str(q) + "\n"
+    file.write(line)
+    q_next = q / (0.98 * p + 2 * q)
+    q_next=round(q_next,2)
+    p_next = 1 - q_next
+    q = q_next
+    p = p_next
+    gen = gen + 1
+
+file.close()
